536c680103a1fe37f9b0db58d55e84a95594327f
madajabbar22 <madajabbar22@gmail.com>
2022-07-02 17:40:14 +0700

    fix kasir

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'admin/dashboard';
+    public const HOME = 'admin/kasir';
 
     /**
      * The controller namespace for the application.
